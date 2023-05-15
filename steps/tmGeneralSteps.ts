ac15bb8aa20245791f62d6737cb5203583991ed9
Kumar Karthikeyan <kumar.karthikeyan@trademe.co.nz>
2023-05-15 22:57:54 +1200

    cucumber feature tweaks

--- steps/tmGeneralSteps.ts
+++ steps/tmGeneralSteps.ts
@@ -1,19 +1,20 @@
 import { Given, When, Then } from "@cucumber/cucumber"
 import { expect, test } from '../fixture/baseTestFixture';
 
-Given('I navigate to the Trade Me home page', async function ({ page, baseURL, homePage, searchResultsPage }) {
+
+Given('I navigate to the Trade Me home page', async ({homePage}) => {
     // Write code here that turns the phrase above into concrete actions
     console.log("Given");
-    // navigate to the Trade Me home page
     await homePage.gotoTradeMeHomePage();
+
 });
 
-When('I search value a Marketplace item on the home page', async function () {
+When('I search value a Marketplace item on the home page', async () => {
     // Write code here that turns the phrase above into concrete actions
     console.log("When");
 });
 
-Then('The resulting page\'s URL and the page\'s title contains Marketplace', async function () {
+Then('The resulting page\'s URL and the page\'s title contains Marketplace', async () => {
     // Write code here that turns the phrase above into concrete actions
     console.log("Then");
 });
